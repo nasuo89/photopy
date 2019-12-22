026116d3a6c32b0389eba172ee3aa424e0331c67
nasuo89 <maxconsole89@gmail.com>
2019-12-22 15:55:18 +0900

    ディレクトリ存在確認実装

--- photopy.py
+++ photopy.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 import datetime
 
-DEST_PATH = "./img_dst/"
+DEST_DIR = "./img_dst/"
 
 #os.system('ls')
 
@@ -11,5 +11,10 @@ p = pathlib.Path("./img_src/test.jpg")
 dt = datetime.datetime.fromtimestamp(p.stat().st_ctime) 
 day_string = dt.strftime('%Y-%m-%d')
 
-make_dir = "mkdir " + DEST_PATH + day_string
-os.system(make_dir)
+dest_path = DEST_DIR + day_string
+
+if os.path.exists(dest_path) :
+    print("already exists")
+else :
+    make_dir = "mkdir " + dest_path
+    os.system(make_dir)   
