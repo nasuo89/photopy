75034188a57e727a5f44cdf79d672c0225b2312f
nasuo89 <maxconsole89@gmail.com>
2019-12-22 16:13:26 +0900

    複数ファイル対応

--- photopy.py
+++ photopy.py
@@ -6,15 +6,17 @@ DEST_DIR = "./img_dst/"
 
 #os.system('ls')
 
-p = pathlib.Path("./img_src/test.jpg")
+p = pathlib.Path("./img_src/")
+p = p.iterdir()
 
-dt = datetime.datetime.fromtimestamp(p.stat().st_ctime) 
-day_string = dt.strftime('%Y-%m-%d')
-
-dest_path = DEST_DIR + day_string
-
-if os.path.exists(dest_path) :
-    print("already exists")
-else :
-    make_dir = "mkdir " + dest_path
-    os.system(make_dir)   
+for idx in p :
+    dt = datetime.datetime.fromtimestamp(idx.stat().st_birthtime) 
+    day_string = dt.strftime('%Y-%m-%d')
+    print(day_string)
+    dest_path = DEST_DIR + day_string
+    
+    if os.path.exists(dest_path) :
+        print("already exists")
+    else :
+        make_dir = "mkdir " + dest_path
+        os.system(make_dir)   
